6a4e6932ac91218689c1ccd910a2068e252af2e9
raqueldiases <raquel123dias.cvc@gmail.com>
2023-05-29 21:36:54 -0300

    acrescentei metodo main apenas para teste

--- t07/src/main/java/com/github/raqueldiases/ufg/poo/Livro.java
+++ t07/src/main/java/com/github/raqueldiases/ufg/poo/Livro.java
@@ -1,5 +1,7 @@
 package com.github.raqueldiases.ufg.poo;
 
 public class Livro {
-    
+    public static void main(String[] args) {
+        System.out.println("Minha primeira aplicacao");
+    }
 }
