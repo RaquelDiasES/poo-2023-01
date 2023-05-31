aedd1438efb710b60527075fc5c4c850ff401f91
raqueldiases <raquel123dias.cvc@gmail.com>
2023-05-31 19:56:58 -0300

    a

--- t07/src/main/java/com/github/raqueldiases/ufg/poo/Pessoa.java
+++ t07/src/main/java/com/github/raqueldiases/ufg/poo/Pessoa.java
@@ -1,10 +1,12 @@
 package com.github.raqueldiases.ufg.poo;
 
+import javax.xml.crypto.Data;
+
 public class Pessoa {
     private short dia;
     private short mes;
     private short ano;
     public short idade;
     private Data nascimento;
-    private String nome;
+    private
 }
