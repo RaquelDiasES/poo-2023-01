80aa65836741306054b563754cbcadcfa6dd54d4
Aluno <raqueldiases>
2023-06-14 20:12:59 -0300

    a

---
+++ t07/src/main/java/com/github/raqueldiases/ufg/poo/T07/Uniao.java
@@ -0,0 +1,17 @@
+package com.github.raqueldiases.ufg.poo.T07;
+
+import java.util.ArrayList;
+
+public class Uniao {
+    private ArrayList filhos;
+    private Pessoa parceiros[2];
+
+    public void novoFilho(Pessoa p){
+        filhos.add(p);
+    }
+
+    public Uniao(Pessoa a, Pessoa b){
+        parceiros[0] = a;
+        parceiros[1] = b;
+    }
+}
