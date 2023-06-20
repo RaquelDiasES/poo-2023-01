df0da7551d6e030197e46e9880c574bbe5738b01
raqueldiases <raquel123dias.cvc@gmail.com>
2023-06-20 19:51:31 -0300

    t08

---
+++ t08/src/main/java/com/github/raqueldiases/ufg/poo/Pais.java
@@ -0,0 +1,11 @@
+package com.github.raqueldiases.ufg.poo;
+
+import java.util.ArrayList;
+
+public class Pais{
+    private Cidade cidade;
+    
+    public void setCidade(Cidade cidade){
+        this.cidade = cidade;
+    }
+}
