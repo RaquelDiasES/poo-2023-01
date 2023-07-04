239c2a89108de2ed0b94b2c2d2a118e045818b88
raqueldiases <raquel123dias.cvc@gmail.com>
2023-07-03 21:21:44 -0300

    h

--- t08/src/main/java/com/github/raqueldiases/ufg/poo/Arquivo.java
+++ t08/src/main/java/com/github/raqueldiases/ufg/poo/Arquivo.java
@@ -2,10 +2,6 @@ package com.github.raqueldiases.ufg.poo;
 
 ppublic class Arquivo {
 
-    
 private Permissao permissao;
 
-    public void setPermissao(Permissao permissao) {
-        this.permissao = permissao;
-    }
 }
