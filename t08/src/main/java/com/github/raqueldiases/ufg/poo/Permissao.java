fd93b31dcd6716e9b34767aa29d796c6190e09dd
raqueldiases <raquel123dias.cvc@gmail.com>
2023-07-03 21:03:29 -0300

    a

--- t08/src/main/java/com/github/raqueldiases/ufg/poo/Permissao.java
+++ t08/src/main/java/com/github/raqueldiases/ufg/poo/Permissao.java
@@ -2,7 +2,7 @@ package com.github.raqueldiases.ufg.poo;
 
 public class Permissao {
     private Grupo grupos;
-    private Arquivo arquivos;
+    private Arquivo arquivo;
 
     public Permissao(Grupo grupo, Arquivo arquivo) {
         this.grupo = grupo;
