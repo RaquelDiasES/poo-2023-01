4d720fd8f7f0b5a02097e0e358ea23cb603ea515
raqueldiases <raquel123dias.cvc@gmail.com>
2023-07-10 18:55:29 -0300

    j

--- t18/src/main/java/com/github/raqueldiases/ufg/poo/T18/Jogador.java
+++ t18/src/main/java/com/github/raqueldiases/ufg/poo/T18/Jogador.java
@@ -1,10 +1,15 @@
 package main.java.com.github.raqueldiases.ufg.poo.T18;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jogador {
     private String nome;
+    private List<Lance> lances;
 
     public Jogador(String nome) {
         this.nome = nome;
+        this.lances = new ArrayList<>();
     }
 
     public String getNome() {
@@ -14,4 +19,13 @@ public class Jogador {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void adicionarLance(Lance lance) {
+        lances.add(lance);
+    }
+
+    public List<Lance> getLances() {
+        return lances;
+    }
+
 }
