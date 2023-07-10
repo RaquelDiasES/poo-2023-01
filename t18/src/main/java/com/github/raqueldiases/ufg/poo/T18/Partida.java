da40729dcbb2dd865732b61dae3e33a30252d3d2
raqueldiases <raquel123dias.cvc@gmail.com>
2023-07-10 17:58:56 -0300

    a

---
+++ t18/src/main/java/com/github/raqueldiases/ufg/poo/T18/Partida.java
@@ -0,0 +1,17 @@
+package com.github.raqueldiases.ufg.poo.T18;
+
+public class Partida {
+    private Tabuleiro tabuleiro;
+
+    public Partida() {
+        this.tabuleiro = new Tabuleiro();
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public void setTabuleiro(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+}
